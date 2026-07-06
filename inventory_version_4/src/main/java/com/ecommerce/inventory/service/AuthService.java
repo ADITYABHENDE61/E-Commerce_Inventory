1e34b80506a4836c6541de9cf756a06ffebd961b
ADITYABHENDE61 <adityabhende61@gmail.com>
2026-07-06 23:58:26 +0530

    Add files via upload

---
+++ inventory_version_4/src/main/java/com/ecommerce/inventory/service/AuthService.java
@@ -0,0 +1,60 @@
+package com.ecommerce.inventory.service;
+
+import com.ecommerce.inventory.dto.AuthResponse;
+import com.ecommerce.inventory.dto.LoginRequest;
+import com.ecommerce.inventory.dto.RegisterRequest;
+import com.ecommerce.inventory.entity.User;
+import com.ecommerce.inventory.repository.UserRepository;
+import com.ecommerce.inventory.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("User already exists");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String token = jwtService.generateToken(user);
+
+        return new AuthResponse(token);
+    }
+}
